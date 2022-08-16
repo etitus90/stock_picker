e9a40ab66beca2c8c48c4f1b37408e438ea93f9e
etitus90 <etitus@protonmail.com>
2022-08-16 13:41:04 -0700

    Initiate variables to 0, frame methodology

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,3 +1,10 @@
 def stock_picker(price_array)
-
+    buy_day = 0
+    sell_day = 0
+    buy_price = 0
+    sell_price = 0
+    profit = 0
+    stock_picker.each_with_index do |value, index|
+        
+    end
 end

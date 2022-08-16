40deef6429cc699ab32727c5007e87a901500383
etitus90 <etitus@protonmail.com>
2022-08-16 13:55:37 -0700

    Confirm necessary variables

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,10 +1,11 @@
 def stock_picker(price_array)
     buy_day = 0
-    sell_day = 0
-    buy_price = 0
-    sell_price = 0
+    sell_day = 1
+    buy_price = price_array[0]
+    sell_price = price_array[1]
     profit = 0
-    stock_picker.each_with_index do |value, index|
+    current_profit = 0
+    stock_picker.each_with_index do |value, day|
         
     end
 end

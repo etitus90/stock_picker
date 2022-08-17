ef98e00f24e87f86206158f4e07f11b5a189fe5d
etitus90 <etitus@protonmail.com>
2022-08-17 13:31:57 -0700

    Finalize code

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,11 +1,23 @@
 def stock_picker(price_array)
-    buy_day = 0
-    sell_day = 1
-    buy_price = price_array[0]
-    sell_price = price_array[1]
+    min_price = price_array[0]
+    min_day= 0
     profit = 0
-    current_profit = 0
-    stock_picker.each_with_index do |value, day|
+    days = [0, 0]
+
+    price_array.each_with_index do |value, day|
+        if value < min_price
+            min_price = value
+            min_day = day
+            next
+        end
         
+        if value - min_price > profit
+            profit = value - min_price
+            days = [min_day, day]
+        end
     end
+
+    days
 end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
